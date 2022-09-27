e41ae97735642f7660f7a94a1655616323c1d083
Ada <96547482+Apexofacircle@users.noreply.github.com>
2022-09-27 08:11:18 -0400

    update

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,13 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "15%";
-    document.getElementById("main").style.marginLeft = "10%";
+    document.getElementById("main").style.marginLeft = "15%";
+    document.getElementById("content").style.marginLeft = "15%";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("content").style.marginLeft = "0";
 }
 window.onload = function() {
     closeNav();
